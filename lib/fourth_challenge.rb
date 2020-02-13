419dca2937ed72d3f44db84c58b96cdaa49bcb0e
Lester Young <lyoung070789@gmail.com>
2020-02-13 23:37:16 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,12 +5,12 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        :friend_1 => {
+        {
           name: "Benovolio",
           age: "17",
           attitude: "worried"
         },
-        :friend_2 => {
+        {
           name: "Mercutio",
           age: "18",
           attitude: "hot-headed"
@@ -22,8 +22,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        :friend_1 => {name: "Steven", age: "30", attitude: "confused"},
-        :friend_2 => {name: "Nurse", age: "44",attitude: "worried"}
+        {name: "Steven", age: "30", attitude: "confused"},
+        {name: "Nurse", age: "44",attitude: "worried"}
       ]
    }
 }
