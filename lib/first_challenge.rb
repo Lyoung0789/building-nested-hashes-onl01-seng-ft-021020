7c07c74a638e1b690c989f45816c0ad0e9299366
Lester Young <lyoung070789@gmail.com>
2020-02-13 23:19:00 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,15 +1,15 @@
 def first_challenge
    epic_tragedy = {
      :montague => {
-       :patriarch => {}
-       :matriarch => {}
-       :hero => {}
+       :patriarch => {},
+       :matriarch => {},
+       :hero => {},
        :her_friends => []
      },
      :capulet => {
-       :patriarch => {}
-       :matriarch => {}
-       :heroine => {}
+       :patriarch => {},
+       :matriarch => {},
+       :heroine => {},
        :heroine_friends => []
      }
      
